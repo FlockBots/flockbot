1c68826eef62b21ca40c0fb3ab287c8ae72f01d9
Stan Janssen <stanjanssen@outlook.com>
2015-07-19 15:37:47 +0200

    version 1.0.3dev added features

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flockbot',
-    version='1.0.2dev',
+    version='1.0.3dev',
     author='Stan Janssen',
     author_email='stanjanssen@outlook.com',
     url='https://github.com/FlockBots/flockbot',
