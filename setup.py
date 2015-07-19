4ad30766690fc0e3da82879bd2494c8565ca703c
Stan Janssen <stanjanssen@outlook.com>
2015-07-19 14:36:39 +0200

    version 1.0.2dev - couple of bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flockbot',
-    version='1.0.1dev',
+    version='1.0.2dev',
     author='Stan Janssen',
     author_email='stanjanssen@outlook.com',
     url='https://github.com/FlockBots/flockbot',
