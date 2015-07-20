c5c111d2f7d23f3d1cec963929e2232faab96dc8
Stan Janssen <stanjanssen@outlook.com>
2015-07-20 21:30:02 +0200

    v1.0.5dev - minor change in interface

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flockbot',
-    version='1.0.4dev',
+    version='1.0.5dev',
     author='Stan Janssen',
     author_email='stanjanssen@outlook.com',
     url='https://github.com/FlockBots/flockbot',
